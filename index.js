5e7fc80717254e713ada8bdb1597a10750601595
Prakhar Trivedi <prakhar0706@gmail.com>
2021-06-25 21:41:12 +0800

    Made basic telegram connection

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+//Discord
 const Discord = require('discord.js')
 require('dotenv').config();
 const discordBot = new Discord.Client();
+const PREFIX = 'o!'
 
-discordBot.on('ready', ()=> {
+discordBot.on('ready', () => {
     console.log('OneStop is ready to serve!')
 })
 
+discordBot.on('message', (msg) => {
+    if (!msg.content.startsWith(PREFIX)) return
+    let args = msg.content.substring(PREFIX.length).split(' ');
+    switch (args[0]) {
+        case 'info':
+            msg.reply('Hey there! I am OneStop!')
+            break;
+    }
+})
+
 discordBot.login(process.env.DISCORD_BOT_TOKEN)
+
+//Telegram
+const { Telegraf } = require('telegraf')
+const oneStopBotToken = process.env.TELEGRAM_BOT_TOKEN
+
+const telegramBot = new Telegraf(oneStopBotToken)
+
+telegramBot.start((ctx) => {
+    ctx.reply('Hey there! I am OneStop!')
+})
+
+telegramBot.launch()
