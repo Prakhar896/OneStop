7a95dbea1005a98c736a07d187d0a83903d36323
Prakhar Trivedi <prakhar0706@gmail.com>
2021-06-26 12:48:50 +0800

    Added covid package
    Set up basic telegram bot commands using telegraf library, still learning how to operate it

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const Discord = require('discord.js')
 require('dotenv').config();
 const discordBot = new Discord.Client();
+const covid19 = require('owid-covid')
 const PREFIX = 'o!'
 
 discordBot.on('ready', () => {
@@ -27,7 +28,34 @@ const oneStopBotToken = process.env.TELEGRAM_BOT_TOKEN
 const telegramBot = new Telegraf(oneStopBotToken)
 
 telegramBot.start((ctx) => {
+    console.log(ctx.from)
     ctx.reply('Hey there! I am OneStop!')
+})
+
+telegramBot.on('sticker', (ctx) => {
+    ctx.reply('👌🏻')
+})
+
+telegramBot.command('devinfo', async (ctx) => {
+    if (ctx.from.id != 1622650771 && ctx.from.id != 1604074166) return ctx.reply('Sorry, you are not a developer of this bot.')
+    try {
+        await ctx.reply(`id: ${telegramBot.botInfo.id}`)
+        await ctx.reply(`first_name: ${telegramBot.botInfo.first_name}`)
+        await ctx.reply(`can_read_all_group_messages: ${telegramBot.botInfo.can_read_all_group_messages}`)
+        await ctx.reply(`can_join_groups: ${telegramBot.botInfo.can_join_groups}`)
+        await ctx.reply(`supports_inline_queries: ${telegramBot.botInfo.supports_inline_queries}`)
+        await ctx.reply(`username: ${telegramBot.botInfo.username}`)
+        await ctx.reply(`is_bot: ${telegramBot.botInfo.is_bot}`)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+telegramBot.command('covid', (ctx) => {
+    covid19.getStats('SGP')
+        .then(stats => {
+            
+        })
 })
 
 telegramBot.launch()
