311e5f501961c41127eaf7922371f5a677f272d9
yisian <yisiankevin@gmail.com>
2021-08-22 16:37:16 +0800

    Location Fix for Telegram, Discord Thumbnail for Weather

--- commands/discord/dWeather.js
+++ commands/discord/dWeather.js
@@ -7,14 +7,10 @@ module.exports = {
     async execute(msg, args, Discord, bot) {
         var location = ''
         if (!args[1]) {
-            location = 'Singapore'
+            location = 'Singapore, SG'
         }
         else {
             location = args.slice(1).join(' ')
-            console.log(location)
-
-            
-
         }
         weather.find({search: location, degreeType: 'C'} , function(err, weatherResult) {
             if(err) console.log(err);
@@ -23,18 +19,18 @@ module.exports = {
         )}
 }
 
-
 function sendWeatherEmbed(msg, weatherResult) {
     try {
         const embedWeather = new Discord.MessageEmbed()
             var current = weatherResult[0].current
             var location = weatherResult[0].location
-        
-            
-            var message = "Weather in " + location.name + ": "
-            message += '\n'+current.skytext + ':  '+ current.temperature + '°C'
-        
-            msg.channel.send(message)
+            var weatherIcon = current.imageUrl 
+            embedWeather.setTitle(`Weather for ${location.name}`)
+            embedWeather.setDescription(`${current.skytext}: ${current.temperature}°C`)
+            embedWeather.setColor(0x00AE86)
+            embedWeather.setThumbnail(weatherIcon)
+            embedWeather.setFooter(`Last Updated: ${current.day}, ${current.observationtime}`)
+            msg.channel.send(embedWeather)
     } catch (err) {
         console.log('DISC, Error in sending Weather Embed: ' + err)
         msg.channel.send(`Error: Please enter the full name of country/city, for Example: "osaka, jp"`)
