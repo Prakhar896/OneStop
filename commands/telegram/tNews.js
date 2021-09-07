40a34d3426986e9ad3abf7ce9155dc173f404c0d
Prakhar Trivedi <prakhar0706@gmail.com>
2021-09-07 17:34:08 +0800

    removed the newsapi package
    trying out google news scraper packages instead

--- commands/telegram/tNews.js
+++ commands/telegram/tNews.js
@@ -3,42 +3,12 @@ const { Telegraf } = require('telegraf')
 const models = require('../../models'); // Use if needed
 
 // Import any extra libraries here
-const NewsAPI = require('newsapi');
-
-const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
 
 module.exports = {
     name: 'News',
-    description: 'Fetches and displays the latest news from CNA',
+    description: 'Fetches and displays the latest news.',
     async execute(ctx, Telegraf, telegramBot) {
         // Add code here
-        args = ctx.message.text.split(' ');
         
-        var category = ''
-        if (!args[1]) {
-            category = 'technology'
-        } else {
-            category = args[1]
-        }
-
-        var query = ''
-        if (!(args.slice(2).join(' '))) {
-            query = ''
-        } else {
-            query = args.slice(2).join(' ')
-        }
-
-        var params = {
-            country: 'sg',
-            category: category,
-            language: 'en'
-        }
-        if (query != '') {
-            params.q = query
-        }
-        
-        newsapi.v2.topHeadlines(params).then(response => {
-            console.log(response.articles[0]);
-          });
     }
 }
